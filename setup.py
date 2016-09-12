352533ba9bcb1b82d7f87cc3a33faa3a73441a2e
Andy Papa <andy.papa@autistici.org>
2016-09-12 12:42:49 -0700

    Added a development environment Vagrantfile

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='Finance Data Scaper API',
     version='1.0.0',
-    url='https://github.com/ajpotato214/Finance-Data-Scraper-API'
+    url='https://github.com/ajpotato214/Finance-Data-Scraper-API',
     packages=['finance_data_scraper', 'finance_data_scraper.scrapers'],
     description="A web-based RESTful API that scrapes various financial websites",
     license='MIT',
     install_requires=[
-        'flask',
-        'flask-restful',
+        'Flask>=0.11.1',
         'tornado',
         'requests',
         'lxml',
-        'memcache'
-    ],
+        'python-memcached',
+        'flask-restful',
+    ]
 )
